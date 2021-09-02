22170166fc30c1c5248964debb6191d90bbcff6a
Ankush Gupta <agupta@medullus.com>
2021-09-02 12:00:33 +0530

    Add project files.

---
+++ GifSplashDemo/GifSplashDemo.Android/SplashScreen.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Timers;
+using Android.App;
+using Android.Content;
+using Android.Content.PM;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.V7.App;
+using Android.Views;
+using Android.Widget;
+using Felipecsl.GifImageViewLibrary;
+
+namespace GifSplashDemo.Droid
+{
+    [Activity(Theme = "@style/splashTheme", MainLauncher = true, NoHistory = true,
+        ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class SplashScreen : AppCompatActivity
+    {
+        private GifImageView gifImageView;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.SplashScreen);
+
+            gifImageView = FindViewById<GifImageView>(Resource.Id.gifImageView);
+
+            Stream input = Assets.Open("mario.gif");
+            byte[] bytes = ConvertFileToByteArray(input);
+            gifImageView.SetBytes(bytes);
+            gifImageView.StartAnimation();
+
+            Timer timer = new Timer();
+            timer.Interval = 5000;
+            timer.AutoReset = false;
+            timer.Elapsed += Timer_Elapsed;
+            timer.Start();
+        }
+
+        private void Timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            StartActivity(new Intent(this, typeof(MainActivity)));
+        }
+
+        private byte[] ConvertFileToByteArray(Stream input)
+        {
+            byte[] buffer = new byte[16 * 1024];
+            using(MemoryStream ms = new MemoryStream())
+            {
+                int read;
+                while ((read = input.Read(buffer, 0, buffer.Length)) > 0)
+                    ms.Write(buffer, 0, read);
+
+                return ms.ToArray();
+            }
+        }
+    }
+}
